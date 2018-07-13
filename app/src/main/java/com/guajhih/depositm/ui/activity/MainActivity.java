fdfcad6037aa3a0bb6b881b9d57675f0b897d877
james <james@januarytc.com>
2018-07-13 17:07:09 +0800

    - 添加fragment

--- app/src/main/java/com/guajhih/depositm/ui/activity/MainActivity.java
+++ app/src/main/java/com/guajhih/depositm/ui/activity/MainActivity.java
@@ -4,6 +4,10 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,9 +15,18 @@ import android.view.MenuItem;
 
 import com.guajhih.depositm.R;
 import com.guajhih.depositm.databinding.ActivityMainBinding;
+import com.guajhih.depositm.ui.fragment.HomeFragment;
+import com.guajhih.depositm.ui.fragment.MoneyBoxFragment;
+import com.guajhih.depositm.ui.fragment.NotifyFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    public static final int TAB_MONEY_BOX = 0;
+    public static final int TAB_HOME = 1;
+    public static final int TAB_NOTIFY = 2;
 
     private ActivityMainBinding binding;
     private Toolbar toolbar;
@@ -26,31 +39,79 @@ public class MainActivity extends AppCompatActivity {
         toolbar = binding.includeToolbar.myToolbar;
         setSupportActionBar(toolbar);
         setBottomNavigationView();
+        setViewPager();
+    }
+
+    private void setViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(MoneyBoxFragment.newInstance("", ""));
+        adapter.addFragment(HomeFragment.newInstance("", ""));
+        adapter.addFragment(NotifyFragment.newInstance("", ""));
+        binding.viewpager.setAdapter(adapter);
+        binding.viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        binding.viewpager.setCurrentItem(TAB_HOME);
     }
 
     private void setBottomNavigationView() {
         bottomNavigationView = binding.includeBottomNavigationView.bottomNavigation;
-        bottomNavigationView.setSelectedItemId(R.id.action_home);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Log.d(TAG, item.getItemId() + " item was selected-------------------");
-                // you can return false to cancel select
-                int id = 0;
                 switch (item.getItemId()) {
-                    case R.id.action_favorites:
-                        id = 0;
+                    case R.id.action_money_box:
+                        binding.viewpager.setCurrentItem(TAB_MONEY_BOX);
                         break;
                     case R.id.action_home:
-                        id = 1;
+                        binding.viewpager.setCurrentItem(TAB_HOME);
                         break;
                     case R.id.action_notify:
-                        id = 2;
+                        binding.viewpager.setCurrentItem(TAB_NOTIFY);
                         break;
                 }
-//                vp.setCurrentItem(id, false);// 改变的 ViewPager 的当前页面
                 return true;
             }
         });
     }
+
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment) {
+            mFragmentList.add(fragment);
+        }
+
+    }
 }
+
+
---
+++ app/src/main/java/com/guajhih/depositm/ui/fragment/MoneyBoxFragment.java
@@ -0,0 +1,60 @@
+package com.guajhih.depositm.ui.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.guajhih.depositm.R;
+
+
+public class MoneyBoxFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public MoneyBoxFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static MoneyBoxFragment newInstance(String param1, String param2) {
+        MoneyBoxFragment fragment = new MoneyBoxFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_money_box, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
